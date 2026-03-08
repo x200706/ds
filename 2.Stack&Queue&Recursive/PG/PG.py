8a93891bcc80efda85af8495225e92c1fbbfbdb5
chi200706 <chi200706@gmail.com>
2026-03-08 15:57:20 +0000

    第二part PG AC

--- 2.Stack&Queue&Recursive/PG/PG.py
+++ 2.Stack&Queue&Recursive/PG/PG.py
@@ -1,3 +1,13 @@
-"""
-這題好像UVA做過
-"""
+def cycle_length(n):
+    if n == 1:
+        return 1
+    if n % 2 == 1:
+        return cycle_length(3 * n + 1) + 1
+    else:
+        return cycle_length(n // 2) + 1
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    print(cycle_length(n))
