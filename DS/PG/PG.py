6f5fa7512ad2d9fc4cad72500957f441e74e128c
chi200706 <chi200706@gmail.com>
2026-03-08 15:15:49 +0000

    勘誤：PG兩次都WA

--- DS/PG/PG.py
+++ DS/PG/PG.py
@@ -1,9 +1,11 @@
 import sys
 
 for line in sys.stdin:
-    s = line.strip()
-    if not s:
+    s = line.rstrip('\n')
+    if len(s) == 0:
         break
     n = len(s)
+    # 核心邏輯：以每個位置k為首，拼接後面+前面的字符
     res = [s[k:] + s[:k] for k in range(n)]
+    # 單空格分隔輸出
     print(' '.join(res))
